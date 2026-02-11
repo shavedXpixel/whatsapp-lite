523d45eb2d73afb57ad21666394c98f3cdc0bae5
PRIYANSU DASH <100258436+shavedXpixel@users.noreply.github.com>
2026-02-11 16:39:19 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 // YOUR BACKEND URL
-const SOCKET_URL = "https://whatsapp-backend-xv12.onrender.com"; 
+const SOCKET_URL = "https://atomic-kay-shavedxpixel-924084a9.koyeb.app"; 
 
 export const socket = io.connect(SOCKET_URL, {
     transports: ["websocket"], // ⚡⚡ FORCE FAST CONNECTION (No Polling)
